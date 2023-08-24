693bd41ddaaed39063e93e880ae8c893810df18c
ElisaIanda <elisaianda01@gmail.com>
2023-08-24 16:12:50 +0200

    Creato header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header class="py-4">
+        <div class="container">
+            <div class="d-flex align-items-center">
+                <div>
+                    <img src="../assets/img/images.jpg" alt="">
+                </div>
+                <h4>YU-GI-OH</h4>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variable" as *;
+
+header {
+    background-color: #f6f6f6;
+
+    img {
+        width: 25%;
+    }
+}
+</style>
+
