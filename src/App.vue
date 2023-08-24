6c52787afacff7c6ad71a473cce5368dc422b8a5
ElisaIanda <elisaianda01@gmail.com>
2023-08-24 17:01:16 +0200

    problema axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 
 import TheHeader from "./components/TheHeader.vue";
+import Card from "./components/Card.vue";
+import CardList from "./components/CardList.vue";
 
 export default {
-    components: { TheHeader }
+    components: { TheHeader, CardList }
 }
 </script>
 
 <template>
 
   <TheHeader></TheHeader>
+  <CardList></CardList>
   <main>
 
   </main>
---
+++ src/components/CardList.vue
@@ -0,0 +1,35 @@
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            card:[]
+        }
+    },
+
+    methods: {
+        returnCard(){
+            const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0"
+
+            axios.get(url).then((response) =>{
+
+                this.card = response.data.results;
+            })
+        }
+    },
+    mounted(){
+        this.returnCard()
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variable" as *;
+
+</style>
+
